50b51c0a124a0594788e8796b7319c1377819dba
Alexander <puch399@yandex.ru>
2024-05-03 14:22:06 +1100

    Добавлены события на кнопки.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+        val libraryBtn = findViewById<Button>(R.id.library_btn)
+        val settingsBtn = findViewById<Button>(R.id.settings_btn)
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку ПОИСК!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchBtn.setOnClickListener(searchClickListener)
+
+        libraryBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку МЕДИАТЕКА!", Toast.LENGTH_SHORT).show()
+        }
+        settingsBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку НАСТРОЕК!", Toast.LENGTH_SHORT).show()
         }
     }
 }
