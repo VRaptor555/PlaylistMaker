a92c48bd7747711b6bf56d207e3e706c32622ac1
Alexander <puch399@yandex.ru>
2024-06-26 16:06:44 +1100

    Сделан поиск композиций через API 2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,11 +2,9 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
@@ -46,6 +44,7 @@ class SearchActivity : AppCompatActivity() {
         .build()
     private val trackService = retrofit.create(TracksApi::class.java)
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -64,7 +63,7 @@ class SearchActivity : AppCompatActivity() {
         backBtn.setOnClickListener {
             this.finish()
         }
-        refreshBtn.setOnClickListener{
+        refreshBtn.setOnClickListener {
             startSearch()
         }
 
@@ -81,8 +80,7 @@ class SearchActivity : AppCompatActivity() {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 startSearch()
                 true
-            } else
-            {
+            } else {
                 false
             }
         }
@@ -116,42 +114,49 @@ class SearchActivity : AppCompatActivity() {
         textValue = savedInstanceState.getString(SEARCH_TEXT, "")
     }
 
-    private fun startSearch(){
+    private fun startSearch() {
         if (inputEditText.text.isNotEmpty()) {
-            trackService.search(inputEditText.text.toString()).enqueue(object : Callback<TrackResponse>{
-                override fun onResponse(
-                    call: Call<TrackResponse>,
-                    response: Response<TrackResponse>
-                ) {
-                    if (response.code()==200){
-                        tracks.clear()
-                        if (response.body()?.results?.isNotEmpty() == true){
-                            tracks.addAll(response.body()?.results!!)
-                            adapter.notifyDataSetChanged()
-                            showMessage("")
+            trackService.search(inputEditText.text.toString())
+                .enqueue(object : Callback<TrackResponse> {
+                    @SuppressLint("NotifyDataSetChanged")
+                    override fun onResponse(
+                        call: Call<TrackResponse>,
+                        response: Response<TrackResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            tracks.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                                showMessage("")
+                            } else {
+                                showMessage("", notFound = true)
+                            }
                         } else {
-                            showMessage( "", notFound = true)
+                            showMessage(response.code().toString(), notConnect = true)
                         }
-                    } else {
-                        showMessage(response.code().toString(), notConnect = true)
                     }
-                }
 
-                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                    showMessage(t.message.toString(), notConnect = true)
-                }
-            })
+                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        showMessage(t.message.toString(), notConnect = true)
+                    }
+                })
         }
     }
-    private fun showMessage(additionalMessage: String, notConnect: Boolean = false, notFound: Boolean = false) {
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showMessage(
+        additionalMessage: String,
+        notConnect: Boolean = false,
+        notFound: Boolean = false
+    ) {
         if (notConnect || notFound) {
             tracks.clear()
             adapter.notifyDataSetChanged()
             if (notConnect) {
                 placeholderFound.visibility = View.GONE
                 placeholderConnect.visibility = View.VISIBLE
-            }
-            else {
+            } else {
                 placeholderConnect.visibility = View.GONE
                 placeholderFound.visibility = View.VISIBLE
             }
@@ -168,42 +173,6 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         private const val SEARCH_TEXT = "SEARCH_TEXT"
         private const val TEXT_VALUE = ""
-        private val TRACK_LIST = listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
     }
 
 }
