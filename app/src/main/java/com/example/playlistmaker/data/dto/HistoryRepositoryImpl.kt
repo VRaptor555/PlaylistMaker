0a4f6e20bad088ab62597c9481ced3dbfbcbc8ef
Alexander <puch399@yandex.ru>
2024-11-10 00:55:03 +1100

    Спринт 15 окончание

--- app/src/main/java/com/example/playlistmaker/data/dto/HistoryRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/data/dto/HistoryRepositoryImpl.kt
@@ -8,47 +8,13 @@ import com.google.gson.Gson
 class HistoryRepositoryImpl(private val shared: SharedPreferences, private val historyKey: String): HistoryRepository {
     override fun read(): Array<Track> {
         val json = shared.getString(historyKey, null) ?: return emptyArray<Track>()
-        return dtoToTrack(Gson().fromJson(json, Array<TrackDto>::class.java))
+        return Converter.dtoToTrack(Gson().fromJson(json, Array<TrackDto>::class.java))
     }
 
     override fun write(writeList: Array<Track>) {
-        val json = Gson().toJson(trackToDto(writeList))
+        val json = Gson().toJson(Converter.trackToDto(writeList))
         shared.edit()
             .putString(historyKey, json)
             .apply()
-    }
-
-    private fun trackToDto(trackArray: Array<Track>): Array<TrackDto> {
-        return trackArray.map {
-            TrackDto(
-                trackId = it.trackId,
-                trackName = it.trackName,
-                artistName = it.artistName,
-                collectionName = it.collectionName,
-                releaseDate = it.releaseDate,
-                primaryGenreName = it.primaryGenreName,
-                country = it.country,
-                trackTimeMillis = it.trackTimeMillis,
-                artworkUrl100 = it.artworkUrl100,
-                previewUrl = it.previewUrl
-            )
-        }.toTypedArray()
-    }
-
-    private fun dtoToTrack(trackDtoArray: Array<TrackDto>): Array<Track> {
-        return trackDtoArray.map {
-            Track(
-                trackId = it.trackId,
-                trackName = it.trackName,
-                artistName = it.artistName,
-                collectionName = it.collectionName,
-                releaseDate = it.releaseDate,
-                primaryGenreName = it.primaryGenreName,
-                country = it.country,
-                trackTimeMillis = it.trackTimeMillis,
-                artworkUrl100 = it.artworkUrl100,
-                previewUrl = it.previewUrl
-            )
-        }.toTypedArray()
     }
 }
